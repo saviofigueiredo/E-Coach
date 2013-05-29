ed596b38bfc69f12fcc3b77d098ada80e01f62be
Savio Figueiredo <saviofigueiredo@outlook.com>
2013-05-28 22:06:24 -0300

    Fixing app.config file and the context class

--- E-Coach/ContextHelper.cs
+++ E-Coach/ContextHelper.cs
@@ -4,13 +4,6 @@
     
     public class ContextHelper : DbContext
     {
-        public ContextHelper()
-        : base(@"data source=localhost; 
-                 initial catalog=ECOACH;
-                 integrated security=true;")
-        {
-        }
-
         public DbSet<Coach> Coaches { get; set; }
         public DbSet<Athlete> Athletes { get; set; }
         public DbSet<Activity> Activities { get; set; }
